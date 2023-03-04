3fff16735bd6e3d3abdd0a78d65d8e3ee964aa48
SohamGhugare <soham.ghugare@gmail.com>
2023-03-04 13:54:46 +0530

    Added OCR

--- backend/api.py
+++ backend/api.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
+from ocr import OCR
+from io import BytesIO
+import requests
+from discord import File as DFile
+from PIL import Image
+import pytesseract
 
 app = FastAPI(
+    title="ViExams API",
     debug=True
 )
 
-@app.get("/")
+ocr = OCR()
+
+@app.get("/api/")
 async def index():
     return {"data": "root"}
+
+@app.post("/api/upload")
+async def upload_image(file: UploadFile = File()):
+    # contents = file.file.read()
+
+    byt = BytesIO(requests.get("https://imgur.com/a/XZLb79k").content)
+    contents = file.file.read()
+    with open(file.filename, "wb") as f:
+        f.write(contents)
+    with open(file.filename, "rb") as f:    
+        return {"data": f.read()}
 
 if __name__ == "__main__":
     uvicorn.run("api:app", reload=True)
---
+++ backend/database.py
@@ -0,0 +1,7 @@
+"""
+    This file contains all the Database configurations
+"""
+
+class Database:
+    def __init__(self) -> None:
+        self.uri = "sqlite:///./data/data.db"

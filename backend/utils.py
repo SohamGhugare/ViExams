6c2d3291fbfbffd57f42e75a19da0451d6cf22d4
SohamGhugare <soham.ghugare@gmail.com>
2023-03-04 17:21:29 +0530

    Added Paper Distinction Utility

---
+++ backend/utils.py
@@ -0,0 +1,23 @@
+"""
+    This file contains all the utility code
+"""
+
+from ocr import OCR
+from PIL import Image
+
+class OcrUtility:
+    def parse_course(self, img_path=None, content=None):
+        if img_path:
+            img = Image.open(img_path)
+            extract = OCR().ocr(img)
+            courses = [
+                "Engineering Chemistry",
+                "Calculus"
+            ]
+            for course in courses:
+                if course in extract:
+                    return course
+                
+if __name__ == "__main__":
+    print(OcrUtility().parse_course(img_path="backend/tests/calculus-jpg.jpg"))
+

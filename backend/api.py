2b866d9c959ea55d44bfd121aec68e2940713c15
SohamGhugare <soham.ghugare@gmail.com>
2023-03-05 13:27:34 +0530

    Added Error Handlers

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 import uvicorn
 import random
 
@@ -17,6 +17,11 @@ app = FastAPI(
 @app.get("/api/papers")
 async def fetch_papers(limit: int=5, course: str=None):
     links = db.fetch_links(course)
+    if not links:
+        raise HTTPException(
+            status_code=404,
+            detail="Course Not Found"
+        )
     if len(links) > limit:
         l = []
         while len(l) < limit:
@@ -36,20 +41,25 @@ async def fetch_papers(limit: int=5, course: str=None):
 
 @app.post("/api/upload")
 async def upload_image(file: UploadFile = File()):
+    try:
+        contents = file.file.read()
+        img_path = f"backend/cache/{file.filename}"
+        with open(img_path, "wb") as f:
+            f.write(contents)
 
-    contents = file.file.read()
-    img_path = f"backend/cache/{file.filename}"
-    with open(img_path, "wb") as f:
-        f.write(contents)
+        wh_url = "https://discord.com/api/webhooks/1081793607926816808/BzjWmFl3D1fkftDM50A_J2Ek-M8oP4BOJPB5TqlpgzrSxUnFajBug0ynYwTJaxGrc1JY"
+        await wh_handler.send_webhook(webhook_url=wh_url, image_path=img_path)
+        
 
-    wh_url = "https://discord.com/api/webhooks/1081793607926816808/BzjWmFl3D1fkftDM50A_J2Ek-M8oP4BOJPB5TqlpgzrSxUnFajBug0ynYwTJaxGrc1JY"
-    await wh_handler.send_webhook(webhook_url=wh_url, image_path=img_path)
-    
-
-    return {"response": {
-        "status": 200,
-        "data": "Successfully Uploaded!"
-    }}
+        return {"response": {
+            "status": 200,
+            "data": "Successfully Uploaded!"
+        }}
+    except:
+        raise HTTPException(
+            status_code=422,
+            detail="Unprocessable Entity - Invalid Image Format / Non-parseable Image Uploaded"
+        )
                                                                                                 
 
 if __name__ == "__main__":
